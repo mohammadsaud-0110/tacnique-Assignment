1123717a14efed04729790ea1a10b4f1e66591a0
Mohammad Saud <mdsaud.mrj@gmail.com>
2023-10-08 16:04:53 +0530

    Blacklisting fixed

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,27 +1,32 @@
 const jwt = require("jsonwebtoken");
+const { BlackList } = require("../models/user.model")
 require("dotenv").config();
 
 // Middleware for user authentication using JWT
-const authenticate = (req, res, next) => {
+const authenticate = async (req, res, next) => {
+
     // Extract the token from the request headers
     const token = req.headers.authorization;
 
-    if (token) {
-        // Verify the token using the accessToken and secret key
-        jwt.verify(token, process.env.accessToken, (err, decoded) => {
-            if (decoded) {
-                // If token is valid, attach the user's ID to the request body
-                req.body.userId = decoded.userId;
-                next(); // Continue with the next middleware or route handler
-            } else {
-                // If token is not valid, respond with a 401 Unauthorized status
-                res.status(401).send("Login again.");
-            }
-        });
-    } else {
-        // If no token is provided, respond with a 401 Unauthorized status
-        res.status(401).send("Not logged in!");
+    if (!token) {
+        return res.status(400).send({ "message": "Token Not Found", success: false });
     }
+
+    const isPresent = await BlackList.exists({ token });
+    if (isPresent) {
+        res.status(401).send("Login again.");
+    }
+    // Verify the token using the accessToken and secret key
+    jwt.verify(token, process.env.accessToken, (err, decoded) => {
+        if (decoded) {
+            // If token is valid, attach the user's ID to the request body
+            req.body.userId = decoded.userId;
+            next(); // Continue with the next middleware or route handler
+        } else {
+            // If token is not valid, respond with a 401 Unauthorized status
+            res.status(401).send("Login again.");
+        }
+    });
 }
 
 // Export the authenticate middleware for use in other parts of the application
